b3798d26e88178e3b2ba4fc28574425ff6b29c71
Szymon Łukaszewicz <szymon.lukaszewicz@fastwhitecat.com>
2019-08-02 19:00:22 +0200

    Initial

---
+++ registartion.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author    Mageviper <admin@mageviper.com>
+ * @copyright 2019 Mageviper
+ * @since     1.0.0
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Mageviper_Instagam',
+    __DIR__
+);
